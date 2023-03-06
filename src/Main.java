3e89b4711bfd871e419714fbe81b5db2f4bee2a3
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-03-06 19:36:43 +0100

    add file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+
     public static void main(String[] args) {
+        File inputFile = new File ("res/in.txt");
+
+        BufferedReader inputFileReader = new BufferedReader(new FileReader(inputFile));
+
 
     }
 }
